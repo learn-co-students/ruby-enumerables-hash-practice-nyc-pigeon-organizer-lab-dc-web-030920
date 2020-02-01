ed38665edc2426f35c65dc2a13c5857a730ea5a9
Alessandro Duterte <alexduterte@gmail.com>
2020-02-01 02:24:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  data.each do |key, value|
+    value.each do |key2, name|
+      name.each do |name|
+        if !result[name]
+          result[name] = {}
+        end
+
+        if !result[name][key]
+          result[name][key] = []
+        end
+
+        result[name][key] << key2.to_s
+      end
+    end
+  end
+  pp result
+  result
+
 end
