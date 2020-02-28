fdf3fccb8c8744823a120aa3deaece058d8b30b0
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-28 16:15:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+pigeons = {}
+data.each do
+    |data_key,data_value|
+    data_value.each do
+      |attribute,names|
+      names.each do
+        |name|
+        if !pigeons[name]
+              pigeons[name] = {}
+        end
+        if !pigeons[name][data_key]
+              pigeons[name][data_key] = []
+        end
+        pigeons[name][data_key].push(attribute.to_s)
+      end
+    end
+  end
+pigeons
+  end
